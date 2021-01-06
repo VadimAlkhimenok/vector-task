0ac0f02a5c61b604e767a305e8ea44fa2722df4b
Vadim <vad.alkhimenok@gmail.com>
2021-01-06 11:00:29 +0300

    FEATURE: add constructor in project

--- src/main/java/main/java/Vector.java
+++ src/main/java/main/java/Vector.java
@@ -1,5 +1,13 @@
 package main.java;
 
 public class Vector {
+    double x;
+    double y;
+    double z;
 
+    public Vector(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
