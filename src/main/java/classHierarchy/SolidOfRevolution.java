151123db982862525816f0c2d9bdcfd9da5947cf
Vadim <vad.alkhimenok@gmail.com>
2021-01-06 20:33:16 +0300

    FEATURE: add solid of revolution class

---
+++ src/main/java/classHierarchy/SolidOfRevolution.java
@@ -0,0 +1,18 @@
+package classHierarchy;
+
+public class SolidOfRevolution extends Shape {
+    private double radius;
+
+    public SolidOfRevolution(double radius, double volume) {
+        super(volume);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
