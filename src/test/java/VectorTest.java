65f902b186c87409bf0bc1adafa8ab656497b5ad
Vadim <vad.alkhimenok@gmail.com>
2021-01-06 11:16:02 +0300

    FEATURE: add test for length of vector method

--- src/test/java/VectorTest.java
+++ src/test/java/VectorTest.java
@@ -2,5 +2,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 class VectorTest {
+    Vector vector = new Vector(2, 2, 2);
+    Vector testVector = new Vector(3, 3, 3);
+
+    @Test
+    public void lengthOfVector() {
+        double actual = vector.lengthOfVector();
+        double expected = 3.4641016151377544;
+        Assertions.assertEquals(expected, actual);
+    }
 
 }
