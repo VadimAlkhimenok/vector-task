0f616479bfc909bfe925b0e5f75ccd6556b81e07
Vadim <vad.alkhimenok@gmail.com>
2021-01-06 11:15:21 +0300

    FEATURE: initial test of vector

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,3 @@
-package main.java;
-
 public class App {
     public static void main(String[] args) {
         Vector[] newVectors = Vector.createVectorsRandom(5);
--- src/main/java/Vector.java
+++ src/main/java/Vector.java
@@ -1,5 +1,3 @@
-package main.java;
-
 public class Vector {
     double x;
     double y;
