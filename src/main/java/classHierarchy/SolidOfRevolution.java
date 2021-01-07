cd2c1227e30d2319ce286f8b66ffef7b40e684c7
Vadim <vad.alkhimenok@gmail.com>
2021-01-07 12:58:31 +0300

    FEATURE: add cylinder class

--- src/main/java/classHierarchy/SolidOfRevolution.java
+++ src/main/java/classHierarchy/SolidOfRevolution.java
@@ -3,26 +3,31 @@ package classHierarchy;
 public class SolidOfRevolution extends Shape {
     private double radius;
 
-    public SolidOfRevolution(double radius, double volume) {
+    public SolidOfRevolution(double volume, double radius) {
         super(volume);
         this.radius = radius;
     }
 
-    public SolidOfRevolution () {}
-
     public double getRadius() {
         return radius;
-    }
-
-    public void setRadius(double radius) {
-        this.radius = radius;
     }
 }
 
 class Ball extends SolidOfRevolution {
-    public Ball (double radius) {
-        super(Math.PI * Math.pow(radius, 3) * 4 / 3, radius);
+    public Ball(double radius) {
+        super(Math.PI * Math.pow(radius, 3) * 4 / 3, radius); // ????
+    }
+}
+
+class Cylinder extends SolidOfRevolution {
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(Math.PI * Math.pow(radius, 2) * height, radius);
+        this.height = height;
     }
 
-    public Ball () {}
+    public double getHeight() {
+        return height;
+    }
 }
