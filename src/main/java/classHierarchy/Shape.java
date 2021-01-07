290013a723464c6c011a26e57d3e13bb2557bfe8
Vadim <vad.alkhimenok@gmail.com>
2021-01-07 14:31:55 +0300

    FIX: update shape class

--- src/main/java/classHierarchy/Shape.java
+++ src/main/java/classHierarchy/Shape.java
@@ -7,13 +7,7 @@ public class Shape {
         this.volume = volume;
     }
 
-    public Shape() {}
-
     public double getVolume() {
         return volume;
-    }
-
-    public void setVolume(double volume) {
-        this.volume = volume;
     }
 }
