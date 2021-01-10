8a1b6fd61222b771e71c5dedbcb23dbc9807e90c
Vadim <vad.alkhimenok@gmail.com>
2021-01-10 18:46:29 +0300

    UPDATE: update enum

--- src/main/java/classHierarchy/TypeOfShapes.java
+++ src/main/java/classHierarchy/TypeOfShapes.java
@@ -1,7 +1,26 @@
 package classHierarchy;
 
 public enum TypeOfShapes {
-    BALL,
-    CYLINDER,
-    PYRAMID
+    BALL (1),
+    CYLINDER (2),
+    PYRAMID (3);
+
+    private int numberShape;
+
+    TypeOfShapes(int numberShape) {
+        this.numberShape = numberShape;
+    }
+
+    public void setNumberShape(int numberShape) {
+        this.numberShape = numberShape;
+    }
+
+    public int setNumberShape() {
+        return numberShape;
+    }
+
+    @Override
+    public String toString() {
+        return "{title: " + numberShape + "}";
+    }
 }
