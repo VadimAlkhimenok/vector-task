07648db788ab254230efb55ac0cd735b004d79d9
vadzimalkhimionak <vad.alkhimenok@gmail.com>
2021-01-12 13:14:55 +0300

    FIX: delete comments

--- src/main/java/classHierarchy/Factory.java
+++ src/main/java/classHierarchy/Factory.java
@@ -26,31 +26,3 @@ public class Factory {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-//package classHierarchy;
-//
-//import static classHierarchy.TypeOfShapes.*;
-//
-//public class Factory {
-//    public Shape create(TypeOfShapes shape, double ...args) {
-//        if (BALL.equals(shape)) {
-//            return new Ball(args[0]);
-//        } else if (CYLINDER.equals(shape)) {
-//            return new Cylinder(args[0], args[1]);
-//        } else if (PYRAMID.equals(shape)) {
-//            return new Pyramid(args[0], args[1]);
-//        } else {
-//            return null;
-//        }
-//    }
-//}
