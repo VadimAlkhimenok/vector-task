ac2133cbcba576a1272ccfc6c285a0ff47b9aa46
vadzimalkhimionak <vad.alkhimenok@gmail.com>
2021-01-12 12:18:12 +0300

    UPDATE: create factory as singleton

--- src/main/java/classHierarchy/Factory.java
+++ src/main/java/classHierarchy/Factory.java
@@ -3,6 +3,15 @@ package classHierarchy;
 import static classHierarchy.TypeOfShapes.*;
 
 public class Factory {
+    private static Factory factory;
+
+    public static Factory getFactory() {
+        if (factory == null) {
+            factory = new Factory();
+        }
+        return factory;
+    }
+
     public Shape create(TypeOfShapes shape, double ...args) {
         if (BALL.equals(shape)) {
             return new Ball(args[0]);
@@ -15,3 +24,31 @@ public class Factory {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//package classHierarchy;
+//
+//import static classHierarchy.TypeOfShapes.*;
+//
+//public class Factory {
+//    public Shape create(TypeOfShapes shape, double ...args) {
+//        if (BALL.equals(shape)) {
+//            return new Ball(args[0]);
+//        } else if (CYLINDER.equals(shape)) {
+//            return new Cylinder(args[0], args[1]);
+//        } else if (PYRAMID.equals(shape)) {
+//            return new Pyramid(args[0], args[1]);
+//        } else {
+//            return null;
+//        }
+//    }
+//}
