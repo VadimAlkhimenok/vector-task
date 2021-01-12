54d585cc6d71c62781a8fa43ed1110af3de2d5ba
vadzimalkhimionak <vad.alkhimenok@gmail.com>
2021-01-12 11:21:42 +0300

    FIX: add public for SolidOfRevolution class and delete useless method

--- src/main/java/classHierarchy/SolidOfRevolution.java
+++ src/main/java/classHierarchy/SolidOfRevolution.java
@@ -1,13 +1,9 @@
 package classHierarchy;
 
-abstract class SolidOfRevolution implements Shape {
-    double radius;
+abstract public class SolidOfRevolution implements Shape {
+    private double radius;
 
     public SolidOfRevolution(double radius) {
         this.radius = radius;
-    }
-
-    public double getRadius() {
-        return radius;
     }
 }
