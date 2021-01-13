f39640ca3017ad05a66bae51bef7ca342bc3550b
vadzimalkhimionak <vad.alkhimenok@gmail.com>
2021-01-13 15:53:09 +0300

    UPDATE: update inverse method

--- src/main/java/collectionTasks/InverseCollection.java
+++ src/main/java/collectionTasks/InverseCollection.java
@@ -1,27 +1,23 @@
 package collectionTasks;
 
-import classHierarchy.Shape;
-
 import java.util.*;
 
 public class InverseCollection {
 
     public static <String, Shape> Map inverse(Map<String, Shape> collection) {
-        Map<Shape, Set<String> > output = new HashMap<Shape, Set<String> >();
+        HashMap<Shape, Collection<String> > output = new HashMap<>();
 
         for (Map.Entry<String, Shape> entry : collection.entrySet()) {
-            Shape oldVal = entry.getValue();
-            String oldKey = entry.getKey();
-            Set<String> newVal;
+            Collection<String> newVal;
 
-            if (output.containsKey(oldVal)) {
-                newVal = output.get(oldVal);
+            if (output.containsKey(entry.getValue())) {
+                newVal = output.get(entry.getValue());
             } else {
-                newVal = new HashSet<>();
+                newVal = new ArrayList<>();
             }
 
-            newVal.add(oldKey);
-            output.put(oldVal, newVal);
+            newVal.add(entry.getKey());
+            output.put(entry.getValue(), newVal);
         }
 
         return output;
